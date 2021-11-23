ac2a45740ad6e37958ce791c0bdf308f8f943c94
mahanfarzaneh2000 <mahan.farzaneh2@gmail.com>
2021-11-23 11:31:38 +0330

    fixing links- adding price support,about pages

--- backend/landing/urls.py
+++ backend/landing/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from landing.views import index_view
+from landing.views import index_view, pricing_view, support_view, about_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('pricing', pricing_view, name='pricing'),
+    path('support', support_view, name='support'),
+    path('about', about_view, name='about'),
 ]
