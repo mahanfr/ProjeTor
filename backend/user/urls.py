fb4d05178f6b9ebb65433efe6ef75951cd14a77b
hamzeh mahmoodi <hamzehmahmoudi9@gmail.com>
2022-01-03 12:31:49 +0330

    profile view added

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
         template_name="user/login.html"), name="login"),
     path("logout", auth_views.LogoutView.as_view(
         template_name="user/logout.html"), name="logout"),
+    path("profile", views.profile, name="profile"),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from user.forms import UserRegisterForm
+from project.models import Task
+from company.models import Company, JoinRequest
 # Create your views here.
 
 
@@ -12,3 +14,22 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, "user/register.html", {"form": form})
+
+
+def profile(request):
+    company_requests = []
+    user_team = request.user.profile.team_set.all()
+    projects = request.user.profile.company.project_set.filter(
+        team__in=user_team)
+    tasks = Task.objects.filter(project__in=projects)
+    user_requests = JoinRequest.objects.filter(
+        user=request.user)
+    if request.user == request.user.profile.company.admin:
+        company_requests = JoinRequest.objects.filter(
+            company=request.user.profile.company)
+    context = {"tasks": tasks,
+               "projects": projects,
+               "user_requests": user_requests,
+               "company_requests": company_requests
+               }
+    return render(request, "user/profile.html", context=context)
